109aada2a35485277053eda58615e85a23e73aeb
wu-json <jason.wu2153@gmail.com>
2025-12-13 14:22:07 -0800

    feat: tailwind

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
+import "./index.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return <h1 className="text-3xl font-bold text-blue-600">Hello World</h1>;
 }
 
 createRoot(document.getElementById("root")!).render(<App />);
