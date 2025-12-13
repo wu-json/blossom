1a3d9d09fbf16cc9f8d710caba11ec498e7d0440
wu-json <jason.wu2153@gmail.com>
2025-12-13 14:21:03 -0800

    feat: react init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import index from "./index.html";
+
+const server = Bun.serve({
+  routes: {
+    "/": index,
+  },
+  development: {
+    hmr: true,
+    console: true,
+  },
+});
+
+console.log(`Server running at http://localhost:${server.port}`);
