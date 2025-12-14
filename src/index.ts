e2d4ab00a42ff2bfcd88aaedaffb34bc0b17f8c1
wu-json <jason.wu2153@gmail.com>
2025-12-14 00:35:33 -0800

    feat: instructions

--- src/index.ts
+++ src/index.ts
@@ -328,6 +328,7 @@ ${personality}
 
 <instructions>
 If the user gives you a sentence in ${languageName}, translate it into English and break it down word by word, explaining the grammar and vocabulary.${kanaInstruction}
+If the user asks questions in ${languageName}, reply in ${languageName}. Otherwise, use English.
 </instructions>`;
 
         const anthropic = new Anthropic({ apiKey });
